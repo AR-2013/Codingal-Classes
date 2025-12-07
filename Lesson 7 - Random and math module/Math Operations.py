b18e04301a69a65c5a27f84152eb6889483003ee
AR-2013 <anairarandeva@icloud.com>
2025-12-07 12:47:10 +0400

    Math Operations

---
+++ Lesson 7 - Random and math module/Math Operations.py
@@ -0,0 +1,12 @@
+import math
+print('The floor and ceiling values of 28.56 are' + str(math.ceil(28.56)) + ', '
+      + str(math.floor(28.56)))
+
+x = 10
+y = -15
+print("The value of x after copying from the y sign is: " + str(math.copysign(x, y)))
+
+print("Absolute value of -96 and 54 are: "+ str(math.fabs(-96))
+      + ', ' + str(math.fabs(54)))
+
+print("The GCD of 24 and 56 is: " + str(math.gcd(24, 56)))
