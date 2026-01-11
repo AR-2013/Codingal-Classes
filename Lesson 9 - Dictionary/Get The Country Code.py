dbcd433f3d85805eca9ac78d216b3467b6b01637
AR-2013 <anairarandeva@icloud.com>
2026-01-11 12:43:36 +0400

    Get The Country Code

---
+++ Lesson 9 - Dictionary/Get The Country Code.py
@@ -0,0 +1,9 @@
+SPCMcountry_code = {'India' : '3579',
+                    'Australia' : '2580',
+                    'Nepal' : '1675'}
+
+print("Country code for India = ")
+print(SPCMcountry_code.get('India', 'Not Found'))
+
+print("Country code for Japan = ")
+print(SPCMcountry_code.get('Japan', 'Not Found'))
