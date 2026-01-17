f2fd7096cc216318a51c383a2345c60dfca247d0
AR-2013 <anairarandeva@icloud.com>
2026-01-17 12:21:01 +0400

    Homework

---
+++ Lesson 10 - Lists and Arrays/Homework.py
@@ -0,0 +1,7 @@
+set1 = {1, 2, 3, 4, 5, 6, 7, 8}
+set2 = {6, 7, 8, 9, 10, 11, 12}
+
+print(set1)
+print(set2)
+
+print("Now I will remove the common values from the sets: "+str(set1.symmetric_difference(set2)))
