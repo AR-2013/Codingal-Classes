c8a7dfea66dd786e20ea1f6de0b7e1a0ef9c8ab7
AR-2013 <anairarandeva@icloud.com>
2026-01-17 12:09:42 +0400

    Set Intersection

---
+++ Lesson 10 - Lists and Arrays/Set Intersection.py
@@ -0,0 +1,9 @@
+setx = {"red", "orange", "yellow", "green"}
+sety = {"green", "blue", "purple", "pink"}
+
+print(setx)
+print(sety)
+
+print("\n Intersection of two said sets: ")
+setz = setx.intersection(sety)
+print(setz)
