b81b8753a1f3da447bd07a6e54235def212a3f93
AR-2013 <anairarandeva@icloud.com>
2026-02-01 11:14:20 +0400

    Student Class

---
+++ Lesson 13 - Object Oriented Programming/Student Class.py
@@ -0,0 +1,5 @@
+class Student():
+    grade = input("What is your grade: ")
+    name = input("What is your name: ")
+    print("Hi! My name is", name, "and I am in grade", grade, ".")
+ob = Student()
