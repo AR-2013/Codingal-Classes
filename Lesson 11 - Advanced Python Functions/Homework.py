ff5b39a0fce6fe3febd02479e3e46051cb46a8b3
AR-2013 <anairarandeva@icloud.com>
2026-02-01 10:57:22 +0400

    Homework

--- Lesson 11 - Advanced Python Functions/Homework.py
+++ Lesson 11 - Advanced Python Functions/Homework.py
@@ -1,4 +1,25 @@
-listy = [1, 2, 3, 4, 5, 'a', 'b', 'c', 'd', 'e']
-print("This is the first list: ")
-print(listy)
+num = int(input("Enter a number: "))
+
+odd_numbers = []
+even_numbers = []
+
+for i in range(1, num):
+    if i % 2 == 0:
+        even_numbers.append(i)
+    else:
+        odd_numbers.append(i)
+
+print("Odd numbers:", odd_numbers)
+print("Even numbers:", even_numbers)
+
+fruits = ["apple", "banana", "mango", "orange"]
+
+updated_fruits = []
+
+for fruit in fruits:
+    updated_fruits.append(fruit.capitalize())
+
+print("Original fruits list:", fruits)
+print("Updated fruits list:", updated_fruits)
+
 
