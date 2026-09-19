67dc0498f766820da6df440c0ac3d039e0cae056
AR-2013 <anairarandeva@icloud.com>
2026-09-19 11:36:51 +0400

    Homework

---
+++ Lesson 49 - Intro to APIs/Homework.py
@@ -0,0 +1,21 @@
+import requests
+
+url = "https://www.codingal.com/student/dashboard/"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+
+    print("Users:")
+    print()
+
+    for user in data:
+        print("Name:", user["name"])
+        print("Username:", user["username"])
+        print("Email:", user["email"])
+        print("City:", user["address"]["city"])
+        print("-" * 30)
+else:
+    print("Failed to fetch data.")
+    print("Status code:", response.status_code)
